a8386e37a986598567a832a097488f9f6dd84b50
Besart Elezi <besart.elezi@hotmail.com>
2022-09-13 14:06:56 +0200

    updated adventure

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ string confirmName = Console.ReadLine();
 if (confirmName == "yes")
 {
     Console.WriteLine("Well then Adventurer " + userName + ", come with me, adventure awaits!");
+    Console.WriteLine("There is a dragon in front of you, what will you do? Fight or flee?");
+    string playerActionOne = Console.ReadLine();
+    if (playerActionOne == "fight")
+    {
+        Console.WriteLine("The dragon instantly vaporized you with his flaming hot breath, good job " + userName);
+    }
+    else if (playerActionOne == "flee")
+    {
+        Console.WriteLine(
+            "The dragon instantly caught up and crashed into you, squishing you into a sludgy pool of incompetence. Good job " +
+            userName);
+    }
 }
 else if (confirmName == "no")
 {
